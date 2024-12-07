3025750c8bce36bd3ffb4ef4b2e6c6482852c0bd
Asharib706 <masharib706@gmail.com>
2024-12-07 20:21:45 +0500

    port

--- app.py
+++ app.py
@@ -154,4 +154,5 @@ def crawl_facebook_marketplace(city: str, query: str, max_price: int):
             browser.close()
 
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app:app", host="0.0.0.0", port=port)
